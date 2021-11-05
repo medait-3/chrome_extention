c85acff1b78ee910a48c410c6ba38e51ddb6ccaf
meait-3 <tchikh3@@gmail.com>
2021-11-05 01:51:35 +0100

    download image

--- background.js
+++ background.js
@@ -1,43 +1,9 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "download") {
+        for(let i = 0; i < request.data.length; i++){
+            chrome.downloads.download({"url": request.data[i].src});
+        }
 
-function MyImageClick(info, tab){
-    console.log("Clicked an image", info, tab);
-    /*
-    chrome.windows.create({
-        "url": "https://facebook.com/sharer.php?u=" + info.srcUrl + "&display=popup",
-        "type": "popup"
-    })
-*/
-chrome.downloads.download({"url": info.srcUrl})
-}
-
-function MyQuoteClick(info, tab){
-    console.log("Clicked an image", info, tab);
-    /*
-    chrome.windows.create({
-        "url": "https://facebook.com/sharer.php?u=" + info.pageUrl + "&display=popup&quote=" + info.selectionText,
-        "type": "popup"
-    })
-    */
-   chrome.downloads.download(info.srcUrl)
-
-
-}
-
-chrome.contextMenus.create({
-    "title": "Share Image",
-    "contexts": ["image"],
-    "onclick": MyImageClick
-})
-
-chrome.contextMenus.create({
-    "title": "Share Quote",
-    "contexts": ["selection"],
-    "onclick": MyQuoteClick
-})
-
-
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
-    console.log("message", msg)
-    sendResponse({"text": "Received the links"});
-
+        sendResponse("Done")
+    }
 })
